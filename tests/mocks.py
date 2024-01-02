837119214239aef8f9dae0480714397be758538d
Florencia-97 <florrr1997@gmail.com>
2024-01-02 20:02:31 -0300

    Initial commit

---
+++ tests/mocks.py
@@ -0,0 +1,21 @@
+from fluence.libs.announcements.announcements import Announcement
+
+
+class Announced:
+    def __init__(self):
+        super().__init__()
+        self._announcements = []
+
+    def do_something(self, an_announcement):
+        self._announcements.append(an_announcement)
+
+    def announcements(self):
+        return self._announcements
+
+
+class AnnouncementMockA(Announcement):
+    pass
+
+
+class AnnouncementMockB(Announcement):
+    pass
